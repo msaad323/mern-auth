9f2222f4a4046b6ddcb97a8c3d4af5e584b3115d
Mohd Saad <saad500nbd@gmail.com>
2025-05-17 12:04:15 +0530

    connect to db

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to db successfully!"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.status(200).send("Hi from mern auth backend!");
