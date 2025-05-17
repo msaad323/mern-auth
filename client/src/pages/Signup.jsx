6d9d32ad90575743cb8f6cde966eac1413ebe8de
Mohd Saad <saad500nbd@gmail.com>
2025-05-17 16:22:43 +0530

    complete signup functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,32 +1,70 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError('');
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if(data.status === false) {
+        setError(data.message)
+      } 
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-5 max-w-lg m-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
         <input
           type="text"
           className="bg-slate-100 p-3 rounded-lg"
           placeholder="Username"
-          id="Username"
+          id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           className="bg-slate-100 p-3 rounded-lg"
           placeholder="Email"
-          id="Email"
+          id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           className="bg-slate-100 p-3 rounded-lg"
           placeholder="Password"
-          id="Password"
+          id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 uppercase disabled:opacity-80 cursor-pointer">
-          Sign Up
+        <button
+          disabled={isLoading}
+          className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 uppercase disabled:opacity-80 cursor-pointer"
+        >
+          {isLoading ? "Loading..." : "Sign Up"}
         </button>
-        <button className="bg-red-600 p-3 rounded-lg text-white hover:opacity-85 uppercase disabled:opacity-80 cursor-pointer">
+        <button className="bg-red-700 p-3 rounded-lg text-white hover:opacity-85 uppercase disabled:opacity-80 cursor-pointer">
           Continue with Google
         </button>
       </form>
@@ -36,6 +74,7 @@ const Signup = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,4 +6,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(), react()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
